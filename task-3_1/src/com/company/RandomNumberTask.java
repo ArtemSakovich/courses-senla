dc6f0a4b1d07bf0afc5f826d396020885b70584a
Artem Sakovich <artem.sakovich2001@gmail.com>
2021-01-27 18:51:49 +0300

    Added task 3-1 [random number task]

---
+++ task-3_1/src/com/company/RandomNumberTask.java
@@ -0,0 +1,23 @@
+package com.company;
+
+/*
+  Программа выводит на экран случайно сгенерированное трёхзначное натуральное число и сумму его цифр
+*/
+
+public class RandomNumberTask {
+
+    public static void main(String[] args) {
+
+        int randomNumber = (new java.util.Random()).nextInt(999);;
+        int sum = 0;
+
+        System.out.println("Random number: " + randomNumber);
+
+        while (randomNumber != 0) {
+            sum += randomNumber % 10;
+            randomNumber /= 10;
+        }
+
+        System.out.println("Sum: " + sum);
+    }
+}
