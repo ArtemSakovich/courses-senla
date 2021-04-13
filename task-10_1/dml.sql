1adcb2ef5f2d571836dfda02dca7e83081548013
ArtemSakovich <artem.sakovich2001@gmail.com>
2021-04-13 18:09:50 +0300

    Some bugs has been fixed.

--- task-10_1/dml.sql
+++ task-10_1/dml.sql
@@ -1,44 +1,35 @@
-CREATE DATABASE courses_senla_task_10;
+INSERT INTO product (maker, model, type)
+VALUES ('Apple', 'pc_1', 'PC'),
+       ('Acer', 'pc_2', 'PC'),
+       ('HP', 'pc_3', 'PC'),
+       ('DELL', 'pc_4', 'PC'),
+       ('Apple', 'pc_5', 'PC'),
+       ('Apple', 'pc_6', 'PC'),
+       ('Xerox', 'pc_7', 'PC'),
+       ('Canyon', 'printer_1', 'Printer'),
+       ('Xerox', 'printer_2', 'Printer'),
+       ('Ricoh', 'printer_3', 'Printer'),
+       ('Samsung', 'laptop_1', 'Laptop'),
+       ('Apple', 'laptop_2', 'Laptop'),
+       ('Prestigio', 'laptop_3', 'Laptop');
 
-CREATE TABLE product (
-                         maker VARCHAR(10) NOT NULL,
-                         model VARCHAR(50) NOT NULL,
-                         type VARCHAR(50) NOT NULL,
-                         CONSTRAINT product_pk PRIMARY KEY (model),
-                         CONSTRAINT chk_type CHECK (type IN ('PC', 'Laptop', 'Printer'))
-);
+INSERT INTO pc (model, speed, ram, hd, cd, price)
+VALUES ('pc_1', 3700, 8, 256, '8x', 2000),
+       ('pc_2', 2200, 2, 128, '24x', 430),
+       ('pc_3', 2200, 2, 256, '4x', 700),
+       ('pc_4', 1700, 4, 128, '12x', 1000),
+       ('pc_5', 1850, 4, 512, '14x', 2700),
+       ('pc_6', 2000, 8, 128, '20x', 3200),
+       ('pc_7', 2500, 2, 256, '10x', 3000);
 
-CREATE TABLE pc (
-                    code SERIAL PRIMARY KEY,
-                    model VARCHAR(50) NOT NULL,
-                    speed SMALLINT NOT NULL,
-                    ram SMALLINT NOT NULL,
-                    hd REAL NOT NULL,
-                    cd VARCHAR(10),
-                    price DECIMAL(18 , 4 ),
-                    CONSTRAINT pc_fk FOREIGN KEY (model)
-                        REFERENCES product (model)
-);
+INSERT INTO printer (model, color, type, price)
+VALUES ('printer_1', 'y', 'Matrix', 200),
+       ('printer_2', 'y', 'Jet', 150)
+    ('printer_3', 'n', 'Laser', 350);
 
-CREATE TABLE laptop (
-                        code SERIAL PRIMARY KEY,
-                        model VARCHAR(50) NOT NULL,
-                        speed SMALLINT NOT NULL,
-                        ram SMALLINT NOT NULL,
-                        hd REAL NOT NULL,
-                        screen INT NOT NULL,
-                        PRICE DECIMAL(18 , 4 ),
-                        CONSTRAINT laptop_fk FOREIGN KEY (model)
-                            REFERENCES product (model)
-);
 
-CREATE TABLE printer (
-                         code SERIAL PRIMARY KEY,
-                         model VARCHAR(50) NOT NULL,
-                         color CHAR(1) NOT NULL,
-                         type VARCHAR(50) NOT NULL,
-                         price DECIMAL(18 , 4 ) NOT NULL,
-                         CONSTRAINT chk_type CHECK (type IN ('Laser', 'Jet', 'Matrix')),
-                         CONSTRAINT printer_fk FOREIGN KEY (model)
-                             REFERENCES product (model)
-);
+INSERT INTO laptop (model, speed, ram, hd, screen, price)
+VALUES ('laptop_1', '1400', 2, 64, '10', 1100),
+       ('laptop_2', '1150', 4, 128, '11', 900),
+       ('laptop_3', '1700', 6, 32, '9', 1250);
+	
